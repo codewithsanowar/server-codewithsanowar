210c62656e13731ac40dcd640fe6157096e4fb56
codewithsanowar <codewithsanowar@gmail.com>
2026-09-19 13:36:40 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -9,9 +9,9 @@ import { isAuth } from "../middlewares/isAuth.js";
 
 const router = express.Router();
 
-router.post("/user/register", register)
-router.post("/user/verify", verifyUser)
-router.post("/user/login", loginUser)
-router.get("/user/me", isAuth, myProfile)
+router.post("/user/register", register);
+router.post("/user/verify", verifyUser);
+router.post("/user/login", loginUser);
+router.get("/user/me", isAuth, myProfile);
 
 export default router;
